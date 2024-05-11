b5e1e793773d36d9961ec9ebd5b60a4252f17f69
yuanzi911 <162786720+yuanzi911@users.noreply.github.com>
2024-05-11 10:32:43 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '243724fa-ede2-49c4-bc0d-7c1abbeb56c2';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
